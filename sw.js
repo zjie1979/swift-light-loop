6a1751a2e4e08537e1e551eba7e0139b970a78f6
zjie1979 <13360694@qq.com>
2026-08-24 23:45:39 +0800

    Lock mobile zoom app mode

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "swift-light-loop-v1";
-const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
+const CACHE_NAME = "swift-light-loop-v1-nozoom-20260824";
+const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest?v=nozoom-20260824", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
